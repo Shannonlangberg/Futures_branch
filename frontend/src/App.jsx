dbdeab925da1cbaa154bcf9773e2a49285211579
Shannon Langberg <Shannon.langberg@futures.church>
2025-12-02 09:21:09 +1030

    Revert: Use standalone Prayer component directly at /prayer route (full Heartbeat-integrated version)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -399,7 +399,7 @@ function App() {
                   <Route path="/connect-groups" element={<Navigate to="/groups" replace />} />
                   <Route path="/persons/:personId" element={<PersonHealthReport />} />
                   <Route path="/pastoral-care" element={<PastoralCareAppointments />} />
-                  <Route path="/prayer" element={<PrayerWrapper />} />
+                  <Route path="/prayer" element={<Prayer />} />
                   <Route path="/campuses" element={<CampusManagement />} />
                   <Route path="/users" element={<UserManagement />} />
                   <Route path="/notifications" element={<NotificationManager />} />
