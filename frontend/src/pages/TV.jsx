aace5d67d0434fd3c1a5769258cdce8eb079d459
Shannon Langberg <Shannon.langberg@futures.church>
2025-12-02 09:52:37 +1030

    Increase Most Watched left padding to 48px to ensure badge is fully visible

--- frontend/src/pages/TV.jsx
+++ frontend/src/pages/TV.jsx
@@ -183,7 +183,7 @@ const TV = () => {
               </h2>
             </div>
             <div className="w-full overflow-x-auto overflow-y-visible pb-10 scrollbar-hide" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-              <div className="flex gap-5" style={{ paddingLeft: '24px', paddingRight: '24px', minWidth: 'fit-content' }}>
+              <div className="flex gap-5" style={{ paddingLeft: '48px', paddingRight: '24px', minWidth: 'fit-content' }}>
                 {/* Add some placeholders to show layout */}
                 {[...mostWatched, ...createPlaceholders(2)].map((item, index) => {
                   if (item.isPlaceholder) {
